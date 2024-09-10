a633426b16e25b874b877c67a3a2b3ac2c9a6f44
theotaylor <theotaylor2000@gmail.com>
2024-09-10 10:54:16 -0400

    put request successful

--- backend/index.js
+++ backend/index.js
@@ -26,44 +26,40 @@ morgan.token('post-body', function (request, response) {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-body'))
 
-app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
+app.get('/api/persons', (request, response, next) => {
+    Person.find({})
+        .then(persons => {
+            response.json(persons)
+        })
+        .catch(error => next(error))
 })
 
-app.get('/info', async (request, response) => {
+app.get('/info', (request, response, next) => {
     // Count the number of documents in the Person collection
-    const count = await Person.countDocuments({});
-    response.send(`
-        <p>Phonebook has info for ${count} people</p>
-        <p>${(new Date()).toString()}</p>
-    `)
+    Person.countDocuments({})
+        .then(count => {
+            response.send(`
+                <p>Phonebook has info for ${count} people</p>
+                <p>${(new Date()).toString()}</p>
+            `)
+        })
+        .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', async (request, response) => {
-    const id = request.params.id
 
-    // Check if the id is a valid ObjectId
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return response.status(400).send({ error: 'Invalid ID format' })
-    }
-
-    try {
-        const person = await Person.findById(id)
-
-        if (person) {
-            response.json(person)
-        } else {
-            response.status(404).end()
-        }
-    } catch (error) {
-        console.error('Error fetching person by ID:', error)
-        response.status(400).send({ error: 'malformatted id' })
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons/', (request, response) => {
+app.post('/api/persons/', (request, response, next) => {
     const body = request.body
 
     if (!body.name || !body.number) {
@@ -72,28 +68,45 @@ app.post('/api/persons/', (request, response) => {
         })
     }
 
-    Person.findOne({ name: body.name })
-        .then(existingPerson => {
-            if (existingPerson) {
-                return response.status(400).json({ 
-                    error: 'Name already exists in the phonebook' 
-                })
-            }
+    Person.save().then(updatedPerson => {
+        response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 
-            const person = new Person({
-                name: body.name,
-                number: body.number,
-            })
+    // Person.findOne({ name: body.name })
+    //     .then(existingPerson => {
+    //         if (existingPerson) {
+    //             return response.status(400).json({ 
+    //                 error: 'Name already exists in the phonebook' 
+    //             })
+    //         }
 
-            return person.save()
+    //         const person = new Person({
+    //             name: body.name,
+    //             number: body.number,
+    //         })
+
+    //         return person.save()
+    //     })
+    //     .then(savedPerson => {
+    //         response.json(savedPerson)
+    //     })
+    //     .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
         })
-        .then(savedPerson => {
-            response.json(savedPerson)
-        })
-        .catch(error => {
-            console.error('Error saving person:', error)
-            response.status(500).json({ error: 'Internal Server Error' })
-        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -124,54 +137,3 @@ const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-
-
-// app.get('/api/persons', (request, response) => {
-//     response.json(persons)
-// })
-// app.get('/info', (request, response) => {
-//     response.send(`
-//         <p>Phonebook has info for ${persons.length} people</p>
-//         <p>${(new Date()).toString()}</p>
-//     `)
-// })
-// app.get('/api/persons/:id', (request, response) => {
-//     const id = request.params.id
-//     const person = persons.find(p => p.id === id)
-
-//     if (person) {
-//         response.json(person)
-//     } else {
-//         response.status(404).end()
-//     }
-// })
-
-// app.post('/api/persons/', (request, response) => {
-//     let id = Math.floor(Math.random() * 1000)
-//     while(persons.find(person => person.id === id)) {
-//         id = Math.floor(Math.random() * 1000)
-//     }
-//     const person = request.body
-//     person.id = String(id)
-
-//     if (!person.name || !person.number) {
-//         return response.status(400).json({ 
-//             error: 'Name or number is missing' 
-//         })
-//     } 
-//     if (persons.find(p => p.name === person.name)) {
-//         return response.status(400).json({ 
-//             error: 'Name already exists in the phonebook' 
-//         })
-//     }
-
-//     persons = persons.concat(person)
-//     response.json(person)
-// })
-// app.delete('/api/persons/:id', (request, response) => {
-//     const id = request.params.id
-//     persons = persons.filter(person => person.id !== id)
-
-//     response.status(204).end()
-// })
